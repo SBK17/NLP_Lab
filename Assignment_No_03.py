ef56405a53d0cdebc63826c316759315058af915
Shubham Kamandar <93696457+shubhamkamandar@users.noreply.github.com>
2023-11-28 21:56:28 +0530

    Assignment Uploaded on 28/11/2023

---
+++ Assignment_No_03.py
@@ -0,0 +1,38 @@
+0#Assignment no : 3
+#Name : Shubham Bappasaheb Kamandar
+#Batch : B2
+#Roll no : 31
+#Title : Implement Named Entity Recognition(NER) on textual data using SpaCy library for English language.
+
+import spacy
+
+# Load the English language model
+nlp = spacy.load("en_core_web_sm")
+
+def perform_ner(text):
+    # Process the text using SpaCy
+    doc = nlp(text)
+    
+    # Extract named entities and their labels
+    entities = [(ent.text, ent.label_) for ent in doc.ents]
+    
+    return entities
+
+if __name__ == "__main__":
+    # Example text
+    text = "Apple Inc. is planning to open a new store in San Francisco next month."
+
+    # Perform Named Entity Recognition
+    named_entities = perform_ner(text)
+
+    # Print the results
+    print("Named Entities:")
+    for entity, label in named_entities:
+        print(f"{entity} - {label}")
+
+
+# # OUTPUT:
+# Named Entities:
+# Apple Inc. - ORG
+# San Francisco - GPE
+# next month - DATE
